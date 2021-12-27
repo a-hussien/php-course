6700a49e77674350b281237679f47b5dde395b39
ahmed hussien <a.hussien87@gmail.com>
2021-12-28 00:04:06 +0200

    add conditional statement

--- index.php
+++ index.php
@@ -18,10 +18,18 @@
     foreach($products as $key => $value){
 	    echo "<p>The ".$key." costs ".$value."</p>";
     }
-    
+
     echo "<h1>Welcome to ".$name."!</h1>";
     echo "<h2>You have $".$credit." in your wallet.</h2>";
     echo "<p>A car costs ".$products['Car']."</p>";
+
+    echo "<h2>Items you can afford</h2>";
+
+    foreach($products as $key => $value){
+        if($value <= $credit ){
+            echo "<p>".$key."</p>"; 
+        }
+    }
     ?>
   </body>
 </html>
