9c77e8822f14dbbb96b3d592e93a7ed19f77bb0b
ahmed hussien <a.hussien87@gmail.com>
2021-12-28 00:15:15 +0200

    finalize code

--- index.php
+++ index.php
@@ -5,47 +5,38 @@
   </head>
   <body>
     <?php
-    // Using two forward slashes we create comments in php.
-    // Within the php tag, we can insert values into our HTML.
-    $name = "PHP Store";
-    $credit = 1000;
+	    $name = "PHP Store";
+        $credit = 1000; 
 
-    $products['Computer']=750;
-    $products['Car']=15000;
-    $products['iPhone']=1000;
-    $products['Toaster']=75;
+	    echo "<h1>Welcome to ".$name."!</h1>";
+	    echo "<h2>You have $".$credit." in your wallet.</h2>";
 
-    foreach($products as $key => $value){
-	    echo "<p>The ".$key." costs ".$value."</p>";
-    }
+        $products['Computer']=750;
+	    $products['Car']=15000;
+	    $products['iPhone']=1000;
+	    $products['Toaster']=75;
 
-    echo "<h1>Welcome to ".$name."!</h1>";
-    echo "<h2>You have $".$credit." in your wallet.</h2>";
-    echo "<p>A car costs ".$products['Car']."</p>";
+      $taxRate=0.0825;
 
-    echo "<h2>Items you can afford</h2>";
+      function tax_calc($amount,$tax){
+	    	$addedTax = $amount*$tax;
+	    	$amountWithTax = round($amount+$addedTax,2);
+	    	return $amountWithTax;
+    	}
 
-    foreach($products as $key => $value){
-        if($value <= $credit ){
-            echo "<p>".$key."</p>"; 
-        }
-    }
+	    foreach($products as $key => $value){
+	    	$costWithTax = tax_calc($value,$taxRate);
+		    echo "<p>The ".$key." costs ".$costWithTax." with tax</p>";
+	    }
 
-    $amount=800;
-    $taxRate=0.0825;
-    $addedTax= $amount*$taxRate;  //amount = 800, tax = .0825
-    echo $addedTax;
+	    echo "<h2>Items you can afford</h2>";
 
-
-    function tax_calc($amount,$tax){
-        $calculate_tax = $amount*$tax;
-        $amount = round($amount+$calculate_tax,2);
-        return $amount;
-    }
-
-    echo "<p>".tax_calc(750,0.223)."</p>";
-
-
+	    foreach($products as $key => $value){
+	    	$costWithTax = tax_calc($value,$taxRate);
+		    if($costWithTax <= $credit ){
+		    	echo "<p>".$key."</p>"; 
+		    }
+	    }
     ?>
   </body>
 </html>
